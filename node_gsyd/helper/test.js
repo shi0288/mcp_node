a3d6fed30fc4f3226b0e06827a03bc07dc1ea6ca
shiqm <shiqm@git.com>
2016-09-13 16:05:48 +0800

    update

--- node_gsyd/helper/test.js
+++ node_gsyd/helper/test.js
@@ -132,6 +132,12 @@ console.log(dateMathUtil.daysBetween('2016-7-7 12:23:33','2016-07-01 00:00:00'))
 //    console.log("err is:" + err);
 //});
 
+var noRepaySql = "SELECT sum(a.money) as allMoney,sum(a.interest) as allInterest,sum(a.st_interest) as AllStInterest" +
+    " from invest_repay a " +
+    " where a.invest_id in (select id from invest b where b.user_id =(SELECT id from user c where c.username='" + "q1" + "')) and a.status=0";
+
+console.log(noRepaySql);
+
 
 //var temp=new Array();
 //temp.push(1);
@@ -263,50 +269,50 @@ console.log(dateMathUtil.daysBetween('2016-7-7 12:23:33','2016-07-01 00:00:00'))
 //    //        console.log(backBodyNode);
 //    //    });
 //    //});
-var loanRepayObjArr = new Array(35);
-var outRepayObjArr = new Array(36);
-var outLoanEveryMonthRepayMoney = calculateService.deMonthSumCal(0.15, 1200000, 36);
-var outCurLoanMoney = 0;
-for (var i = 0; i < 36; i++) {
-    var liveLoanPeriod = i + 1;
-    outRepayObjArr[i] = {};
-    //计算利息
-    var outLoanInterest = calculateService.deMonthInterestCal(0.15, 1200000, outLoanEveryMonthRepayMoney, liveLoanPeriod);
-    //计算本金
-    var out_money = outLoanEveryMonthRepayMoney - outLoanInterest;
-    outCurLoanMoney += out_money;
-    if (liveLoanPeriod == 36) {
-        out_money = out_money + (1200000 - outCurLoanMoney);
-    }
-    outRepayObjArr[i].money = out_money;
-    outRepayObjArr[i].interest = outLoanInterest;
-}
-for (var i = 0; i < 35; i++) {
-    var loanPeriod = i + 1;
-    var outLoanPeriod = 36 - 35 + i;
-    loanRepayObjArr[i] = {};
-    loanRepayObjArr[i].id = "2222" + '_' + (i + 1);
-    loanRepayObjArr[i].repay_day = dateMathUtil.dateMonthAdd('2019-08-05 00:00:00', 5, -35 + 36);
-    if (loanPeriod == 1) {
-        //***日利息=[（第一次返息日-线上起息日）/（第一次返息日-线下起息日）] * 第一期应还利息
-        //计息日到第一次还款日时间差
-        var firstMoneyDaysNum = dateMathUtil.daysBetween(loanRepayObjArr[i].repay_day, '2016-09-09 00:00:00');
-        //上一次线下还款日到第一次还款日时间差
-        var outRepayTime = dateMathUtil.dateMonthAdd('2019-08-05 00:00:00', 5, -35);
-        var firstMoneyOutDaysNum = dateMathUtil.daysBetween(loanRepayObjArr[i].repay_day, outRepayTime) + 1;
-        loanRepayObjArr[i].interest = Math.round(firstMoneyDaysNum / firstMoneyOutDaysNum * outRepayObjArr[outLoanPeriod].interest);
-        loanRepayObjArr[i].money = Math.round(firstMoneyDaysNum / firstMoneyOutDaysNum * outRepayObjArr[outLoanPeriod].money);
-
-    }
-    else {
-        loanRepayObjArr[i].interest = outRepayObjArr[outLoanPeriod].interest;
-        loanRepayObjArr[i].money = outRepayObjArr[outLoanPeriod].money;
-    }
-    loanRepayObjArr[i].loan_id = '2222';
-    loanRepayObjArr[i].period = loanPeriod;
-    loanRepayObjArr[i].all_period = 35;
-    loanRepayObjArr[i].st_interest = 0;
-    loanRepayObjArr[i].status = 0;//0为还款  1已还
-}
-
-console.log(dateMathUtil.daysBetween('2016-09-09 00:00:00', '2016-09-09 00:00:00'));
+//var loanRepayObjArr = new Array(35);
+//var outRepayObjArr = new Array(36);
+//var outLoanEveryMonthRepayMoney = calculateService.deMonthSumCal(0.15, 1200000, 36);
+//var outCurLoanMoney = 0;
+//for (var i = 0; i < 36; i++) {
+//    var liveLoanPeriod = i + 1;
+//    outRepayObjArr[i] = {};
+//    //计算利息
+//    var outLoanInterest = calculateService.deMonthInterestCal(0.15, 1200000, outLoanEveryMonthRepayMoney, liveLoanPeriod);
+//    //计算本金
+//    var out_money = outLoanEveryMonthRepayMoney - outLoanInterest;
+//    outCurLoanMoney += out_money;
+//    if (liveLoanPeriod == 36) {
+//        out_money = out_money + (1200000 - outCurLoanMoney);
+//    }
+//    outRepayObjArr[i].money = out_money;
+//    outRepayObjArr[i].interest = outLoanInterest;
+//}
+//for (var i = 0; i < 35; i++) {
+//    var loanPeriod = i + 1;
+//    var outLoanPeriod = 36 - 35 + i;
+//    loanRepayObjArr[i] = {};
+//    loanRepayObjArr[i].id = "2222" + '_' + (i + 1);
+//    loanRepayObjArr[i].repay_day = dateMathUtil.dateMonthAdd('2019-08-05 00:00:00', 5, -35 + 36);
+//    if (loanPeriod == 1) {
+//        //***日利息=[（第一次返息日-线上起息日）/（第一次返息日-线下起息日）] * 第一期应还利息
+//        //计息日到第一次还款日时间差
+//        var firstMoneyDaysNum = dateMathUtil.daysBetween(loanRepayObjArr[i].repay_day, '2016-09-09 00:00:00');
+//        //上一次线下还款日到第一次还款日时间差
+//        var outRepayTime = dateMathUtil.dateMonthAdd('2019-08-05 00:00:00', 5, -35);
+//        var firstMoneyOutDaysNum = dateMathUtil.daysBetween(loanRepayObjArr[i].repay_day, outRepayTime) + 1;
+//        loanRepayObjArr[i].interest = Math.round(firstMoneyDaysNum / firstMoneyOutDaysNum * outRepayObjArr[outLoanPeriod].interest);
+//        loanRepayObjArr[i].money = Math.round(firstMoneyDaysNum / firstMoneyOutDaysNum * outRepayObjArr[outLoanPeriod].money);
+//
+//    }
+//    else {
+//        loanRepayObjArr[i].interest = outRepayObjArr[outLoanPeriod].interest;
+//        loanRepayObjArr[i].money = outRepayObjArr[outLoanPeriod].money;
+//    }
+//    loanRepayObjArr[i].loan_id = '2222';
+//    loanRepayObjArr[i].period = loanPeriod;
+//    loanRepayObjArr[i].all_period = 35;
+//    loanRepayObjArr[i].st_interest = 0;
+//    loanRepayObjArr[i].status = 0;//0为还款  1已还
+//}
+//
+//console.log(dateMathUtil.daysBetween('2016-09-09 00:00:00', '2016-09-09 00:00:00'));
